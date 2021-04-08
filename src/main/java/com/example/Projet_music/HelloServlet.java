773d27bc2229f2cc6d3b9a70bb1aeacee0ab837b
mazenrikab <mazenrikab3@gmail.com>
2021-04-08 17:03:56 +0200

    serv2

--- src/main/java/com/example/Projet_music/HelloServlet.java
+++ src/main/java/com/example/Projet_music/HelloServlet.java
@@ -9,27 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
-    }
-}
-public class HelloServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
+        message = "Hello Worlds!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
