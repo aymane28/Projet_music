1c621047ecf8295e007208a343140f674e030928
aymane28 <aymanedhj7@gmail.com>
2021-04-08 17:09:12 +0200

    jhfedh

--- src/main/java/com/example/Projet_music/HelloServlet.java
+++ src/main/java/com/example/Projet_music/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello World48!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
