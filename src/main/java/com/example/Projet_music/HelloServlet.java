f435bf364d55847654c340c1637d70eba5b65e97
MohamedESIEA <abdelkader@et.esiea.fr>
2021-04-08 20:56:56 +0200

    3 eme commit

--- src/main/java/com/example/Projet_music/HelloServlet.java
+++ src/main/java/com/example/Projet_music/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World Moha test number 5";
+        message = "Moha test number 5";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
