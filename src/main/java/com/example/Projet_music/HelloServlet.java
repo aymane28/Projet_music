777d8f34ef3d5dbbb138cb840357e7842b25a28d
Aymane <60755817+aymane28@users.noreply.github.com>
2021-04-08 20:48:19 +0200

    Update HelloServlet.java

--- src/main/java/com/example/Projet_music/HelloServlet.java
+++ src/main/java/com/example/Projet_music/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World Aymane!";
+        message = "Hello World mazen";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
